2dedc177e4d547189b4533ba3db90f4a45c50f18
Laura Blasco <113889507+lauuub@users.noreply.github.com>
2023-03-31 10:35:02 +0200

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,15 @@
+package Basico;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		//Comentario de una sola línea
+		
+		/*
+		 * Comentario de más de una línea
+		 */
+	}
+
+}
