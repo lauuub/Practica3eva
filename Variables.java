4c21e152a0287f58488d4820a920a9c7e2b1f3b8
Laura Blasco <113889507+lauuub@users.noreply.github.com>
2023-03-31 10:35:33 +0200

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,29 @@
+package Basico;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		/* TODO Auto-generated method stub
+		 *Variables para enteros
+		 * Dependen de la longitud del número
+		*/
+		
+		byte entero=12;
+		System.out.print("Número entero: "+entero);
+		
+		short entero1=12456;
+		System.out.print("Número entero: "+entero1);
+
+		int entero2=1245656;
+		System.out.print("Número entero: "+entero2);
+		
+		long entero3=124565678;
+		System.out.print("Número entero: "+entero3);
+
+		//Variables con decimales
+		
+		float decimal=3.45f;
+		System.out.print("Número decimal: "+decimal);
+	}
+
+}
